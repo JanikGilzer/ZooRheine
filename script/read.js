182d26d55d087ff3c8ce77afa9e406dbe2b81f3e
JanikGilzer <168821626+JanikGilzer@users.noreply.github.com>
2025-02-22 19:04:23 +0100

    authentication header und vieles mehr

--- script/read.js
+++ script/read.js
@@ -96,7 +96,6 @@ function updateHeaderForGuest() {
 
 function getCookie(name) {
     const value = `; ${document.cookie}`;
-    console.log(document.cookie);
     const parts = value.split(`; ${name}=`);
     if (parts.length === 2) return parts.pop().split(';').shift();
 }
